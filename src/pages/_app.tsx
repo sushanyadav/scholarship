43384c8d781dc04931c0413b4065d4f832f0b83e
Sushan Yadav <sushanyadav98@gmail.com>
2023-01-14 18:53:14 +0545

    init: project setup

---
+++ src/pages/_app.tsx
@@ -0,0 +1,23 @@
+import "@/styles/globals.scss";
+
+import Head from "next/head";
+
+import { CoreLayout } from "@/common/components/CoreLayout";
+import { PageHead } from "@/common/components/PageHead";
+
+export const App = ({ Component, pageProps }) => {
+  const Layout = Component.layout ? Component.layout : CoreLayout;
+  return (
+    <>
+      <Head>
+        <meta content="width=device-width, initial-scale=1.0" name="viewport" />
+        <PageHead />
+      </Head>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </>
+  );
+};
+
+export default App;
