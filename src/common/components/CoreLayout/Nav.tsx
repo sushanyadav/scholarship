262e6c160e560015c8a21aaa235e247cf443ac7f
Sushan Yadav <sushanyadav98@gmail.com>
2023-01-14 21:06:39 +0545

    build fix 5

--- src/common/components/CoreLayout/Nav.tsx
+++ src/common/components/CoreLayout/Nav.tsx
@@ -1,6 +1,6 @@
 import { Button, Flex, Stack } from '@chakra-ui/react';
 
-import { ScholarshipsLogo } from '../IconComponents/ScholarshipsLogo';
+import { ScholarshipsLogo } from '../Icons/ScholarshipsLogo';
 
 export const HORIZONTAL_PADDING = {
   base: 4,
